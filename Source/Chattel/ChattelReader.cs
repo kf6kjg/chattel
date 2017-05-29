00ab3aca708ab226178aa991be483f67472767c3
Ricky Curtice <kf6kjg@users.noreply.github.com>
2017-05-29 14:28:55 -0700

    Split the cache subsystem into its own class
    Likewise isolation of purpose.

--- Source/Chattel/ChattelReader.cs
+++ Source/Chattel/ChattelReader.cs
@@ -35,11 +35,10 @@ using ProtoBuf;
 
 namespace Chattel {
 	public class ChattelReader {
-		private static readonly ILog LOG = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+		//private static readonly ILog LOG = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-		private ChattelConfiguration _config;
-
-		private readonly ConcurrentDictionary<string, StratusAsset> _assetsBeingWritten = new ConcurrentDictionary<string, StratusAsset>();
+		private readonly ChattelConfiguration _config;
+		private readonly ChattelCache _cache;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="T:ChattelReader"/> class.
@@ -51,13 +50,17 @@ namespace Chattel {
 			}
 
 			_config = config;
+
+			if (_config.CacheEnabled) {
+				_cache = new ChattelCache(config);
+			}
 		}
 
 		public StratusAsset GetAssetSync(UUID assetId) {
-			StratusAsset result;
+			StratusAsset result = null;
 
 			// Hit up the cache first.
-			if (TryGetCachedAsset(assetId, out result)) {
+			if (_cache?.TryGetCachedAsset(assetId, out result) ?? false) {
 				return result;
 			}
 
@@ -71,140 +74,12 @@ namespace Chattel {
 				}
 
 				if (result != null) {
-					CacheAsset(result);
+					_cache?.CacheAsset(result);
 					return result;
 				}
 			}
 
 			return null;
-		}
-
-		private bool TryGetCachedAsset(UUID assetId, out StratusAsset asset) {
-			if (!_config.CacheEnabled) {
-				asset = null;
-				return false;
-			}
-
-			// Convert the UUID into a path.
-			var path = UuidToCachePath(assetId);
-
-			if (_assetsBeingWritten.TryGetValue(path, out asset)) {
-				LOG.Debug($"[ASSET_READER] Attempted to read an asset from cache, but another thread is writing it.  Shortcutting read of {path}");
-				// Asset is currently being pushed to disk, so might as well return it now since I have it in memory.
-				return true;
-			}
-
-			// Attempt to read and return that file.  This needs to handle happening from multiple threads in case a given asset is read from multiple threads at the same time.
-			var removeFile = false;
-			try {
-				using (var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read)) {
-					asset = Serializer.Deserialize<StratusAsset>(stream);
-				}
-				return true;
-			}
-			catch (PathTooLongException e) {
-				_config.DisableCache();
-				LOG.Error("[ASSET_READER] Attempted to read a cached asset, but the path was too long for the filesystem.  Disabling caching.", e);
-			}
-			catch (DirectoryNotFoundException) {
-				// Kinda expected if that's an item that's not been cached.
-			}
-			catch (UnauthorizedAccessException e) {
-				_config.DisableCache();
-				LOG.Error("[ASSET_READER] Attempted to read a cached asset, but this user is not allowed access.  Disabling caching.", e);
-			}
-			catch (FileNotFoundException) {
-				// Kinda expected if that's an item that's not been cached.
-			}
-			catch (IOException e) {
-				// This could be temporary.
-				LOG.Warn("[ASSET_READER] Attempted to read a cached asset, but there was an IO error.", e);
-			}
-			catch (ProtoException e) {
-				LOG.Warn($"[ASSET_READER] Attempted to read a cached asset, but there was a protobuf decoding error.  Removing the offending cache file as it is either corrupt or from an older installation: {path}", e);
-				removeFile = true;
-			}
-
-			if (removeFile) {
-				try {
-					File.Delete(path);
-					// TODO: at some point the folder tree should be checked for folders that should be removed.
-				}
-#pragma warning disable RECS0022 // A catch clause that catches System.Exception and has an empty body
-				catch {
-					// If there's a delete failure it'll just keep trying as the asset is called for again.
-				}
-#pragma warning restore RECS0022 // A catch clause that catches System.Exception and has an empty body
-			}
-
-			// Nope, no ability to get the asset.
-			asset = null;
-			return false;
-		}
-
-		private void CacheAsset(StratusAsset asset) {
-			if (!_config.CacheEnabled || asset == null) { // Caching is disabled or stupidity.
-				return;
-			}
-
-			var path = UuidToCachePath(asset.Id);
-
-			if (!_assetsBeingWritten.TryAdd(path, asset)) {
-				LOG.Debug($"[ASSET_READER] Attempted to write an asset to cache, but another thread is already doing so.  Skipping write of {path}");
-				// Can't add it, which means it's already being written to disk by another thread.  No need to continue.
-				return;
-			}
-
-			try {
-				// Since UuidToCachePath always returns a path underneath the cache folder, this will only attempt to create folders there.
-				Directory.CreateDirectory(Directory.GetParent(path).FullName);
-				using (var file = File.Create(path)) {
-					Serializer.Serialize(file, asset);
-				}
-				// Writing is done, remove it from the work list.
-				StratusAsset temp;
-				_assetsBeingWritten.TryRemove(path, out temp);
-				LOG.Debug($"[ASSET_READER] Wrote an asset to cache: {path}");
-			}
-			catch (UnauthorizedAccessException e) {
-				_config.DisableCache();
-				LOG.Error("[ASSET_READER] Attempted to write an asset to cache, but this user is not allowed access.  Disabling caching.", e);
-			}
-			catch (PathTooLongException e) {
-				_config.DisableCache();
-				LOG.Error("[ASSET_READER] Attempted to write an asset to cache, but the path was too long for the filesystem.  Disabling caching.", e);
-			}
-			catch (DirectoryNotFoundException e) {
-				_config.DisableCache();
-				LOG.Error("[ASSET_READER] Attempted to write an asset to cache, but cache folder was not found.  Disabling caching.", e);
-			}
-			catch (IOException e) {
-				// This could be temporary.
-				LOG.Error("[ASSET_READER] Attempted to write an asset to cache, but there was an IO error.", e);
-			}
-		}
-
-		/// <summary>
-		/// Converts a UUID to a path based on the cache location.
-		/// </summary>
-		/// <returns>The path.</returns>
-		/// <param name="id">Asset identifier.</param>
-		private string UuidToCachePath(UUID id) {
-			return UuidToCachePath(id.Guid);
-		}
-
-		/// <summary>
-		/// Converts a GUID to a path based on the cache location.
-		/// </summary>
-		/// <returns>The path.</returns>
-		/// <param name="id">Asset identifier.</param>
-		private string UuidToCachePath(Guid id) {
-			var noPunctuationAssetId = id.ToString("N");
-			var path = _config.CacheFolder.FullName;
-			for (var index = 0; index < noPunctuationAssetId.Length; index += 2) {
-				path = Path.Combine(path, noPunctuationAssetId.Substring(index, 2));
-			}
-			return path + ".pbasset";
 		}
 	}
 }
