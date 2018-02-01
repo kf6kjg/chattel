c45638c1e20c305d4a575846944c33e2f2864ccb
Ricky C <kf6kjg@users.noreply.github.com>
2018-01-31 20:47:34 -0800

    Code coverage by XR.Baboon
    Doesn't work and is very much my-system-only.

---
+++ covem.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+
+cd bin
+
+BABOON_CFG="Chattel.covcfg" mono ../../XR.Baboon/covtool/bin/covem.exe ../../nunit-console/bin/Debug/nunit3-console.exe ChattelTests.dll Chattel-AssetToolsTests.dll
+
+mono ../../XR.Baboon/covtool/bin/cov-srchtml.exe ../Chattel.covcfg.covdb ../Source ../covem.report
