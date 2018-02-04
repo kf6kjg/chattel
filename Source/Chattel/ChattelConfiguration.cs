4c39b09fa68ed729fbde53110761ab1fd83a4ac1
Ricky C <kf6kjg@users.noreply.github.com>
2018-02-03 22:44:50 -0800

    BREAKING CHANGE: Removed the ChattelConfiguration Nini ctor
    The Nini config ctor causes not only an extra dependency, but causes an object ownership problem that results in both a potially heavy memory leak and a problem with the server connectors not closing properly because they never got disposed.
    
    It's now entirely up to the application to handle the configuration.  Which means a pile of work for me, but maybe I'll figure out a clean way to handle it.  I've shoved start of trying to cross the fine line code off into a new branch to moulder.  Not sure it's worthwhile, but I also don't want to lose the idea just yet.

--- Source/Chattel/ChattelConfiguration.cs
+++ Source/Chattel/ChattelConfiguration.cs
@@ -23,12 +23,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Sockets;
-using Nini.Config;
 
 namespace Chattel {
 	public class ChattelConfiguration {
@@ -121,120 +118,6 @@ namespace Chattel {
 		}
 		public ChattelConfiguration(IAssetServer assetServer)
 			: this(null, null, 0, assetServer){
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="T:ChattelConfiguration"/> class.  Can only define servers that will be accessed serially.
-		/// The <paramref name="configSource"/> is the whole Nini configuration, but the <paramref name="assetConfig"/> is the block that contains the following entries:
-		/// <list type="bullet">
-		/// 	<item>
-		/// 		<term>CachePath</term>
-		/// 		<description>String. The path to the folder where the disk cache will be stored.  If empty or missing caching is disabled.</description>
-		/// 	</item>
-		/// 	<item>
-		/// 		<term>Servers</term>
-		/// 		<description>String. Comma delimited list of config section names to pull up server parameters.</description>
-		/// 	</item>
-		/// </list>
-		/// </summary>
-		/// <param name="configSource">Config source.</param>
-		/// <param name="assetConfig">Config instance for the asset options.</param>
-		public ChattelConfiguration(IConfigSource configSource, IConfig assetConfig) {
-			configSource = configSource ?? throw new ArgumentNullException(nameof(configSource));
-
-			// Set up caching
-			var cachePath = assetConfig?.GetString("CachePath", DEFAULT_DB_FOLDER_PATH) ?? DEFAULT_DB_FOLDER_PATH;
-
-			if (string.IsNullOrWhiteSpace(cachePath)) {
-				LOG.Log(Logging.LogLevel.Info, () => $"CachePath is empty, caching assets disabled.");
-			}
-			else if (!Directory.Exists(cachePath)) {
-				LOG.Log(Logging.LogLevel.Info, () => $"CachePath folder does not exist, caching assets disabled.");
-			}
-			else {
-				CacheFolder = new DirectoryInfo(cachePath);
-				LOG.Log(Logging.LogLevel.Info, () => $"Caching assets enabled at {CacheFolder.FullName}");
-			}
-
-			// Set up caching
-			var writeCachePath = assetConfig?.GetString("WriteCacheFilePath", string.Empty) ?? string.Empty;
-			var writeCacheRecordCount = (uint)Math.Max(0, assetConfig?.GetLong("WriteCacheRecordCount", DEFAULT_WRITECACHE_RECORD_COUNT) ?? DEFAULT_WRITECACHE_RECORD_COUNT);
-
-			if (string.IsNullOrWhiteSpace(writeCachePath) || writeCacheRecordCount <= 0 || !CacheEnabled) {
-				LOG.Log(Logging.LogLevel.Warn, () => $"WriteCacheFilePath is empty, WriteCacheRecordCount is zero, or caching is disabled. Crash recovery will be compromised.");
-			}
-			else {
-				WriteCacheFile = new FileInfo(writeCachePath);
-				WriteCacheRecordCount = writeCacheRecordCount;
-				LOG.Log(Logging.LogLevel.Info, () => $"Write cache enabled at {WriteCacheFile.FullName} with {WriteCacheRecordCount} records.");
-			}
-
-			// Set up server handlers
-			var serialParallelAssetServers = new List<List<IAssetServer>>();
-			SerialParallelAssetServers = serialParallelAssetServers;
-
-			// Read in a config list that lists the priority order of servers and their settings.
-			var serialParallelServerSources = assetConfig?
-				.GetString("Servers", string.Empty)
-				.Split(',')
-				.Where(parallelSources => !string.IsNullOrWhiteSpace(parallelSources))
-				.Select(parallelSources => parallelSources
-					.Split('&')
-					.Where(source => !string.IsNullOrWhiteSpace(source))
-					.Select(source => source.Trim())
-				)
-				.Where(parallelSources => parallelSources.Any())
-			;
-
-			if (serialParallelServerSources != null && serialParallelServerSources.Any()) {
-				foreach (var parallelSources in serialParallelServerSources) {
-					var parallelServerConnectors = new List<IAssetServer>();
-					foreach (var source in parallelSources) {
-						var sourceConfig = configSource.Configs[source];
-						IAssetServer serverConnector = null;
-						var type = sourceConfig?.GetString("Type", string.Empty)?.ToLower(System.Globalization.CultureInfo.InvariantCulture);
-						try {
-							switch (type) { // TODO: Create a way for these types to automatically register a string name and parameters with defaults in a central location. Then use that to generically process these here intead of a switch full of one-offs.
-								case "whip":
-									serverConnector = new AssetServerWHIP(
-										source,
-										sourceConfig.GetString("Host", string.Empty),
-										sourceConfig.GetInt("Port", 32700),
-										sourceConfig.GetString("Password", "changeme") // Yes, that's the default password for WHIP.
-									);
-									break;
-								case "cf":
-									serverConnector = new AssetServerCF(
-										source,
-										sourceConfig.GetString("Username", string.Empty),
-										sourceConfig.GetString("APIKey", string.Empty),
-										sourceConfig.GetString("DefaultRegion", string.Empty),
-										sourceConfig.GetBoolean("UseInternalURL", true),
-										sourceConfig.GetString("ContainerPrefix", string.Empty)
-									);
-									break;
-								default:
-									LOG.Log(Logging.LogLevel.Warn, () => $"Unknown asset server type in section [{source}].");
-									break;
-							}
-						}
-						catch (SocketException e) {
-							LOG.Log(Logging.LogLevel.Error, () => $"Asset server of type '{type}' defined in section [{source}] failed setup. Skipping server.", e);
-						}
-
-						if (serverConnector != null) {
-							parallelServerConnectors.Add(serverConnector);
-						}
-					}
-
-					if (parallelServerConnectors.Any()) {
-						serialParallelAssetServers.Add(parallelServerConnectors);
-					}
-				}
-			}
-			else {
-				LOG.Log(Logging.LogLevel.Warn, () => "Servers empty or not specified. No asset server sections configured.");
-			}
 		}
 
 		public bool CacheEnabled {
